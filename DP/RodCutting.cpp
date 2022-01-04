459716370ede261dd1f08d197a7bbbfa1502ce37
Vishesh20155 <88935135+Vishesh20155@users.noreply.github.com>
2022-01-04 14:56:34 +0530

    Add files via upload

---
+++ DP/RodCutting.cpp
@@ -0,0 +1,15 @@
+int cutRod(int price[], int n) {
+        //code here
+        int t[n+1][n+1];
+        for(int i=0; i<n+1; ++i){
+            for(int j=0; j<n+1; ++j){
+                if(i==0 || j==0)
+                t[i][j] = 0;
+                else if(i<=j)
+                t[i][j] = max((price[i-1] + t[i][j-i]), t[i-1][j]);
+                else
+                t[i][j] = t[i-1][j];
+            }
+        }
+        return t[n][n];
+    }
