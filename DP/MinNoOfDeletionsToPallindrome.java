459b4249b70a81d13631870e9a2f7a335d5ebadb
Vishesh20155 <88935135+Vishesh20155@users.noreply.github.com>
2022-01-08 01:06:33 +0530

    Create MinNoOfDeletionsToPallindrome.java
    Given a string 'str' of size ‘n’. The task is to remove or delete the minimum number of characters from the string so that the resultant string is a palindrome. Find the minimum numbers of characters we need to remove.
    Note: The order of characters should be maintained.

---
+++ DP/MinNoOfDeletionsToPallindrome.java
@@ -0,0 +1,31 @@
+class Solution 
+{ 
+    int minDeletions(String text1, int n)
+    {
+        String text2 = "";
+        char ch;
+        for (int i=0; i<text1.length(); i++)
+        {
+            ch= text1.charAt(i); 
+            text2= ch+text2; 
+        }
+        int dp[][] = new int[1002][1002];
+        for (int i = 0; i <= text1.length(); i++) {
+           dp[i][0] = 0;
+        }
+        for (int i = 0; i <= text2.length(); i++) {
+           dp[0][i] = 0;
+        }
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+       return n - dp[text1.length()][text2.length()];
+    }
+} 
