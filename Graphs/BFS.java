bf82f2a6427340c89c765c2784198a737b9385a9
Vishesh20155 <88935135+Vishesh20155@users.noreply.github.com>
2022-01-21 01:19:54 +0530

    Add files via upload

---
+++ Graphs/BFS.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    ArrayList<Integer> ans;
+    Queue<Integer> q;
+    
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ans = new ArrayList<>();
+        q = new LinkedList<>();
+        boolean[] m = new boolean[V];
+        Arrays.fill(m, false);
+        q.add(0);
+        
+        while(!q.isEmpty()){
+            int x = q.poll();
+            
+            ans.add(x);
+            m[x] = true;
+            for (int i : adj.get(x)) {
+                if (m[i] == false){
+                    q.add(i);
+                    m[i] = true;
+                }
+            }
+        }
+        
+        return ans;
+    }
+
+    public void dfsHelp(int n, ArrayList<ArrayList<Integer>> adj){
+        
+    }
+}
